0ace352c556a9138aec89fed38dbb3944d14e3c6
nLesxw <211539653@qq.com>
2023-12-07 15:28:08 +0800

    404 状态码

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") //写入响应头
     if r.URL.Path == "/" {
         fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
     } else if r.URL.Path == "/about" {
         fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
             "<a href=\"mailto:summer@example.com\">summer@example.com</a>")
     } else {
+		w.WriteHeader(http.StatusNotFound) //返回响应状态码
         fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
             "<p>如有疑惑，请联系我们。</p>")
     }
