9554a4c5e72ccc2fad7234d98578eba4d15c99f7
nLesxw <211539653@qq.com>
2023-12-18 11:35:56 +0800

    集成 GORM

---
+++ pkg/model/model.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"GoBlog/pkg/logger"
+	"gorm.io/gorm"
+
+	//GORM 的 MySQL 数据库驱动导入
+	"gorm.io/driver/mysql"
+	
+)
+
+//DB gorm.DB对象
+var DB *gorm.DB
+
+//ConnectDB 初始化模型
+func ConnectDB() *gorm.DB {
+
+	var err error
+	//mysql.Config{DSN: "root:123456@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",}
+	config := mysql.New(mysql.Config{
+		DSN: "root:123456@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+	})
+	//准备数据库连接池
+	DB, err = gorm.Open(config, &gorm.Config{})
+
+	logger.LogError(err)
+
+	return DB
+}
