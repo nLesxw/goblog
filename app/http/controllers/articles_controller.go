c1601e0baebb594b6319746e97a027b9add6b84f
nLesxw <211539653@qq.com>
2023-12-19 11:00:12 +0800

    文章内容页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -43,15 +43,27 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request){
     }else {
         //4. 读取成功,显示文章
 
+        //4.0 设置模板相对路径
+        viewDir := "views"
+
+        //4.1 所有布局模板文件 slice
+        files, err := filepath.Glob(viewDir+"/layouts/*.gohtml")
+        logger.LogError(err)
+
+        //4.2 在 slice 里新增我们的目标文件
+        newFiles := append(files, viewDir+"/articles/show.gohtml")
+
+        //4.3 解析模板文件
         tmpl, err := template.New("show.gohtml").Funcs(
             template.FuncMap{
-                "RouteName2URL" : route.Name2URL,
+                "RouteName2URL": route.Name2URL,
                 "Uint64ToString": types.Uint64ToString,
-            }).ParseFiles("views/articles/show.gohtml")
-       logger.LogError(err)
+            }).ParseFiles(newFiles...)
+        logger.LogError(err)
 
-        err = tmpl.Execute(w, article)
-       logger.LogError(err)
+        //4.4 渲染模板，将所有文章的数据传输进去
+        err = tmpl.ExecuteTemplate(w, "app", article)
+        logger.LogError(err)
     }
 }
 
