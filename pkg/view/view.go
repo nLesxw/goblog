ccb136798e26918d628a0479ab05bbce6e66387e
nLesxw <211539653@qq.com>
2023-12-25 11:31:31 +0800

    登录状态

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"GoBlog/pkg/auth"
 	"GoBlog/pkg/logger"
 	"GoBlog/pkg/route"
 	"html/template"
@@ -13,21 +14,40 @@ import (
 type D map[string]interface{}
 
 //Render 渲染视图
-func Render(w io.Writer, data interface{}, tplFiles ...string){
+func Render(w io.Writer, data D, tplFiles ...string){
 	RenderTemplate(w, "app", data, tplFiles...)
 }
 
 //RenderSimple 渲染简单的视图
-func RenderSimple(w io.Writer, data interface{}, tplFiles ...string){
+func RenderSimple(w io.Writer, data D, tplFiles ...string){
 	RenderTemplate(w, "simple", data, tplFiles...)
 }
 
 //RenderTemplate 渲染视图
-func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string){
-	//1.设置模板相对路径
+func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string){
+	
+	//1. 通用模板数据
+	data["isLogined"] = auth.Check()
+
+	//2. 生成模板文件
+	allFiles := getTemplateFiles(tplFiles...)
+
+	//3. 解析所有模板文件
+	tmpl, err := template.New("").Funcs(template.FuncMap{
+		"RouteName2URL": route.Name2URL,
+	}).ParseFiles(allFiles...)
+	logger.LogError(err)
+	
+	//4. 渲染模板
+	err = tmpl.ExecuteTemplate(w, name, data)
+	logger.LogError(err)
+}
+
+func getTemplateFiles(tplFiles ...string) []string{
+	//1. 设置模板相对路径
 	viewDir := "views/"
 
-	//2. 遍历传参文件列表 Slice，设置正确的路径，支持dir.filename 语法糖
+	//2. 遍历传参文件列表 Slice 设置正确路径，支持dir.filename 语法糖
 	for i, f := range tplFiles {
 		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
 	}
@@ -37,15 +57,5 @@ func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...stri
 	logger.LogError(err)
 
 	//4. 合并所有文件
-	allFiles := append(layoutFiles, tplFiles...)
-
-	//5.解析所有的模板文件
-	tmpl, err := template.New("").Funcs(template.FuncMap{
-		"RouteName2URL": route.Name2URL,
-	}).ParseFiles(allFiles...)
-	logger.LogError(err)
-	
-	//6.渲染模板
-	err = tmpl.ExecuteTemplate(w, name, data)
-	logger.LogError(err)
+	return append(layoutFiles, tplFiles...)
 }
