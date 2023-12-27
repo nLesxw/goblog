2f8cdce9830761c320eb210abbeb382ef6dcd223
nLesxw <211539653@qq.com>
2023-12-27 20:57:52 +0800

    文章作者

--- pkg/model/article/crud.go
+++ pkg/model/article/crud.go
@@ -10,8 +10,7 @@ import (
 func Get(idstr string) (Article, error){
 	var article Article
 	id := types.StringToUint64(idstr)
-	//First是 gorm.DB 提供的用以从结果集中获取第一条数据的查询方法，需要注意的是第二个参数可以传参整型或者字符串 ID，但是传字符串会有 SQL 注入的风险
-	if err := model.DB.First(&article, id).Error; err != nil {
+	if err := model.DB.Preload("User").First(&article, id).Error; err != nil {
 		return article, err
 	}
 
@@ -21,7 +20,7 @@ func Get(idstr string) (Article, error){
 //GetAll 获取全部文章
 func GetAll()([]Article, error) {
 	var articles []Article
-	if err := model.DB.Find(&articles).Error; err != nil {
+	if err := model.DB.Preload("User").Find(&articles).Error; err != nil {
 		return articles, err
 	}
 	return articles, nil
