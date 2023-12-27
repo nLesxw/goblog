864027998525ba8ecaed9982210be42273f02531
nLesxw <211539653@qq.com>
2023-12-27 21:24:31 +0800

    用户的文章

--- pkg/model/user/user.go
+++ pkg/model/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"GoBlog/pkg/model"
 	"GoBlog/pkg/password"
+	"GoBlog/pkg/route"
 )
 
 //User 用户模型
@@ -23,5 +24,5 @@ func (user *User) ComparePassword(_password string) bool{
 
 //Link 方法用来生成用户链接
 func (user User) Link() string {
-	return ""
+	return route.Name2URL("users.show", "id", user.GetStringID())
 }
